07f9608474f822c0d264057e0808a0ce0e9b2ad7
LuisFelipe7215 <170734954+LuisFelipe7215@users.noreply.github.com>
2025-02-08 11:27:25 -0300

    Exer006 e Exer007

---
+++ java-exercicios/src/com/exercicios/test/Exer006.java
@@ -0,0 +1,7 @@
+package com.exercicios.test;
+
+public class Exer006 {
+    public static void main(String[] args) {
+        System.out.println("XXXXX\n" + "X   X\n".repeat(3) + "XXXXX\n");
+    }
+}
---
+++ java-exercicios/src/com/exercicios/test/Exer007.java
@@ -0,0 +1,14 @@
+package com.exercicios.test;
+
+public class Exer007 {
+    public static void main(String[] args) {
+
+        System.out.println("Aluno(A)        NOTA");
+        System.out.println("========        =====");
+        System.out.println("ALINE           9.0");
+        System.out.println("MÀRIO           DEZ");
+        System.out.println("SÈRGIO          4.5");
+        System.out.println("SHIRLEY         7.0");
+
+    }
+}
