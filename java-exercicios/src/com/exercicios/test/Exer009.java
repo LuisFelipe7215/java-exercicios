e29c0c6bb69e91bbae2374836e209208c6657412
LuisFelipe7215 <170734954+LuisFelipe7215@users.noreply.github.com>
2025-02-10 11:13:02 -0300

    Exercicio 9, 10 e 11 realizados

---
+++ java-exercicios/src/com/exercicios/test/Exer009.java
@@ -0,0 +1,13 @@
+package com.exercicios.test;
+
+public class Exer009 {
+    public static void main(String[] args) {
+        System.out.println("    X     X");
+        System.out.println("     X   X");
+        System.out.println("      X X");
+        System.out.println("       X ");
+        System.out.println("      X X");
+        System.out.println("     X   X");
+        System.out.println("    X     X");
+    }
+}
