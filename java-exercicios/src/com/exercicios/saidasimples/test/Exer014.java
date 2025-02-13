8ccb6e1df43d44bc47673bfdefef8d4fa3114776
LuisFelipe7215 <170734954+LuisFelipe7215@users.noreply.github.com>
2025-02-13 10:41:07 -0300

    Refactor Exer014

--- java-exercicios/src/com/exercicios/saidasimples/test/Exer014.java
+++ java-exercicios/src/com/exercicios/saidasimples/test/Exer014.java
@@ -6,18 +6,33 @@ import java.util.List;
 public class Exer014 {
     public static void main(String[] args) {
         List<String> pinheiro = new ArrayList<>();
-        pinheiro.add("       *");
-        pinheiro.add("      ***");
-        pinheiro.add("     *****");
-        pinheiro.add("    *******");
-        pinheiro.add("   *********");
-        pinheiro.add("  ***********");
-        pinheiro.add(" *************");
-        pinheiro.add("***************");
-        pinheiro.add("       **");
-        pinheiro.add("       **");
-        pinheiro.add("      ****");
-        pinheiro.forEach(System.out::println);
+        StringBuilder sb = new StringBuilder();
+
+        pinheiro.add("       X");
+        pinheiro.add("      XXX");
+        pinheiro.add("     XXXXX");
+        pinheiro.add("    XXXXXXX");
+        pinheiro.add("   XXXXXXXXX");
+        pinheiro.add("  XXXXXXXXXXX");
+        pinheiro.add(" XXXXXXXXXXXXX");
+        pinheiro.add("XXXXXXXXXXXXXXX");
+        pinheiro.add("       XX");
+        pinheiro.add("       XX");
+        pinheiro.add("      XXXX");
+
+        for (String string : pinheiro) {
+
+            for (int j = 0; j < string.length(); j++) {
+                if (string.charAt(j) == 'X') {
+                    sb.append("*");
+                } else {
+                    sb.append(" ");
+                }
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
 
     }
 }
