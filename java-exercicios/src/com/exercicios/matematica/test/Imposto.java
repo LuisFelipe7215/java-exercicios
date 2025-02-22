91a9db4b070d89413ec07f4972043487cc1e5eb4
LuisFelipe7215 <afcgamerplays@gmail.com>
2025-02-22 10:39:27 -0300

    Refactor imposto exercise

--- java-exercicios/src/com/exercicios/matematica/test/Imposto.java
+++ java-exercicios/src/com/exercicios/matematica/test/Imposto.java
@@ -2,64 +2,48 @@ package com.exercicios.matematica.test;
 
 public class Imposto {
     public static void main(String[] args) {
-        calculaImpostoRenda(22_000);
+        calculaImpostoRenda(1200);
+        calculaImpostoRenda(2500);
+        calculaImpostoRenda(6000);
+        calculaImpostoRenda(12000);
     }
 
     private static void calculaImpostoRenda(double renda) {
-
-        String rendaFormatada = String.format("%.2f", renda);
-
         double impostoTotal = impostoTotais(renda);
-
         double rendaLiquida = renda - impostoTotal;
 
-        String impostoTotalFormatado = String.format("%.2f", impostoTotal);
-
-        System.out.printf("Você terá que pagar R$%s de impostos%n", impostoTotalFormatado);
-        System.out.printf("Renda bruta: R$%s%n", rendaFormatada);
-        System.out.printf("Renda líquida: R$%s%n", String.format("%.2f", rendaLiquida));
+        System.out.printf("Renda bruta: R$%.2f%n", renda);
+        System.out.printf("Imposto total a pagar: R$%.2f%n", impostoTotal);
+        System.out.printf("Renda líquida após impostos: R$%.2f%n", rendaLiquida);
         System.out.println("----------------------------------------");
     }
 
     private static double impostoTotais(double renda) {
-        double tributo;
         double impostoTotal = 0;
-        String tributoFormatado;
+
+        System.out.println("----------------------------------------");
 
         if (renda <= 1200) {
-            System.out.println("----------------------------------------");
-            System.out.println("Sua renda está na faixa de R$1.200,00");
-            System.out.printf("A renda está isenta de tributação%n");
-            System.out.println("----------------------------------------");
-            return 0;
+            System.out.println("Faixa 1: Isento de impostos.");
+            return 0; // Isento de impostos
         }
 
-        if (renda >= 1201) {
-            System.out.println("----------------------------------------");
-            System.out.println("Sua renda está na faixa de R$1.201,00 até R$5.000,00");
-            tributo = renda * 0.10;
+        if (renda > 1200 && renda <= 5000) {
+            double tributo = (renda - 1200) * 0.10;
             impostoTotal += tributo;
-            tributoFormatado = String.format("%.2f", tributo);
-            System.out.printf("Será cobrado R$%s de imposto%n", tributoFormatado);
-            System.out.println("----------------------------------------");
+            System.out.printf("Faixa 2: (R$1.201 até R$%.2f) - Imposto: R$%.2f%n", renda, tributo);
         }
 
-        if (renda >= 5001) {
-            System.out.println("Sua renda está na faixa de R$5.001,00 até R$10.000,00");
-            tributo = renda * 0.15;
+        if (renda > 5000 && renda <= 10_000) {
+            double tributo = (renda - 5000) * 0.15;
             impostoTotal += tributo;
-            tributoFormatado = String.format("%.2f", tributo);
-            System.out.printf("Será cobrado R$%s de imposto%n", tributoFormatado);
-            System.out.println("----------------------------------------");
+            System.out.printf("Faixa 3: (R$5.001 até R$%.2f) - Imposto: R$%.2f%n", renda, tributo);
         }
 
         if (renda > 10_000) {
-            System.out.println("Sua renda está na faixa acima de R$10.000,00");
-            tributo = renda * 0.20;
+            double tributo = (renda - 10_000) * 0.20;
             impostoTotal += tributo;
-            tributoFormatado = String.format("%.2f", tributo);
-            System.out.printf("Será cobrado R$%s de imposto%n", tributoFormatado);
-            System.out.println("----------------------------------------");
+            System.out.printf("Faixa 4: (Acima de R$10.000) - Imposto: R$%.2f%n", tributo);
         }
 
         return impostoTotal;
