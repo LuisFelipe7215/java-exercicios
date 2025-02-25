ce7adccf0dd9f9f14d497ddd7dcd8a0ebee0f604
LuisFelipe7215 <170734954+LuisFelipe7215@users.noreply.github.com>
2025-02-25 10:48:09 -0300

    Exercicio feito.

---
+++ java-exercicios/src/com/exercicios/matematica/test/FuncoesAninhadas.java
@@ -0,0 +1,10 @@
+package com.exercicios.matematica.test;
+
+public class FuncoesAninhadas {
+    public static void main(String[] args) {
+        double num = Math.sqrt(3);
+        num = Math.round(num);
+        num = Math.exp(num);
+        System.out.printf("A exponencial da raiz de 3 é %.2f", num);
+    }
+}
