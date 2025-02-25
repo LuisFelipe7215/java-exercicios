c34c99af079143574f91b8c7feec012c6aadfd9a
LuisFelipe7215 <afcgamerplays@gmail.com>
2025-02-25 11:00:27 -0300

    Exercicio feito.

---
+++ java-exercicios/src/com/exercicios/matematica/test/DevagarVaiLonge.java
@@ -0,0 +1,8 @@
+package com.exercicios.matematica.test;
+
+public class DevagarVaiLonge {
+    public static void main(String[] args) {
+        double distanciaAnual = (800 * 2 * 5 * 45) / 1000.0;
+        System.out.printf("Andou cerca de %.2fKM durante 1 ano.%n", distanciaAnual);
+    }
+}
