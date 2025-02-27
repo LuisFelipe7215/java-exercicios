ee9febd80e8838e2783f65b7cc7776cfee53f6d9
LuisFelipe7215 <170734954+LuisFelipe7215@users.noreply.github.com>
2025-02-27 11:42:48 -0300

    Exercícios feitos.

---
+++ java-exercicios/src/com/exercicios/variaveis/matematica/Media.java
@@ -0,0 +1,18 @@
+package com.exercicios.variaveis.matematica;
+
+import java.util.Scanner;
+
+public class Media {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double media = 0;
+        for (int i = 1; i <= 3; i++) {
+            System.out.printf("Entre com a nota %d%n", i);
+            media += sc.nextDouble();
+        }
+        media /= 3;
+
+        System.out.println("A média das notas é: " + media);
+    }
+}
