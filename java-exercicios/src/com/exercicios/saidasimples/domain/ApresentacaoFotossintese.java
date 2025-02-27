478f16ef3db673ae5632b907561adf48879cd21b
LuisFelipe7215 <afcgamerplays@gmail.com>
2025-02-27 11:11:21 -0300

    Exercicio refatorado.

--- java-exercicios/src/com/exercicios/saidasimples/domain/ApresentacaoFotossintese.java
+++ java-exercicios/src/com/exercicios/saidasimples/domain/ApresentacaoFotossintese.java
@@ -12,7 +12,8 @@ public class ApresentacaoFotossintese extends JFrame {
             "A água e os sais minerais absorvidos pelas raízes sobem através dos vasos lenhosos do caule e chegam às folhas.",
             "Nas folhas, existe uma substância verde, a clorofila, que absorve a energia luminosa do sol.",
             "Ao mesmo tempo, por meio dos estômatos presentes nas folhas, a planta absorve gás carbônico do ar.",
-            "Usando a energia solar, o gás carbônico e o hidrogênio contido na água retirada do solo,\napós complicadas reações químicas, a planta produz açúcares (glicose)."
+            "Usando a energia solar, o gás carbônico e o hidrogênio contido na água retirada do solo," +
+                    "\napós complicadas reações químicas, a planta produz açúcares (glicose)."
     };
     private int slideAtual = 0;
 
