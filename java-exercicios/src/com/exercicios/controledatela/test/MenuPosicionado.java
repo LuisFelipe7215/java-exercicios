d40bc8572270b9f96df7252ca7fe4f49c7cef5fe
LuisFelipe7215 <afcgamerplays@gmail.com>
2025-02-27 10:47:50 -0300

    Exercicio feito.

---
+++ java-exercicios/src/com/exercicios/controledatela/test/MenuPosicionado.java
@@ -0,0 +1,20 @@
+package com.exercicios.controledatela.test;
+
+import java.util.Scanner;
+
+public class MenuPosicionado {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String espacamento = " ".repeat(20);
+        String menu = "\n".repeat(2) +
+                espacamento + "Menu Relatórios\n" +
+                espacamento + "1 - Por nome\n" +
+                espacamento + "2 - Por código\n" +
+                espacamento + "3 - Por data\n" +
+                espacamento + "4 - Fim\n" + "\n"
+                + "Opção:";
+        System.out.println(menu);
+        sc.nextLine();
+        sc.close();
+    }
+}
