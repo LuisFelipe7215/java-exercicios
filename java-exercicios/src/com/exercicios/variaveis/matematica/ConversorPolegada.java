2f9afd917032a82183d34964547be8e9ef0df973
LuisFelipe7215 <afcgamerplays@gmail.com>
2025-03-06 10:56:13 -0300

    Conversor de cm em polegada v2

--- java-exercicios/src/com/exercicios/variaveis/matematica/ConversorPolegada.java
+++ java-exercicios/src/com/exercicios/variaveis/matematica/ConversorPolegada.java
@@ -9,12 +9,15 @@ public class ConversorPolegada {
         System.out.println("Digite um valor em cm");
         double cm = sc.nextDouble();
 
+        System.out.println("Digite a variação em cm");
+        double variacao = sc.nextDouble();
+
         System.out.printf("%-10s %s%n", "CM", "INCH");
         System.out.println("--------------------");
 
         for (int i = 0; i < 10; i++) {
             System.out.printf("%-10.2f %.2f%n", cm, cm / 2.54);
-            cm += 10;
+            cm += variacao;
         }
 
         sc.close();
