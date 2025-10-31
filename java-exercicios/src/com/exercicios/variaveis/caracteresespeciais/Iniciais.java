71957cdcf29a4d78933781182f13f7ff2be179d0
platinex <luis104.lf@gmail.com>
2025-10-31 16:43:07 -0300

    Modify 1 exercise

--- java-exercicios/src/com/exercicios/variaveis/caracteresespeciais/Iniciais.java
+++ java-exercicios/src/com/exercicios/variaveis/caracteresespeciais/Iniciais.java
@@ -4,15 +4,8 @@ public class Iniciais {
     static void main() {
         String name = "Salameico Maleico";
 
-        char[] chars = name.toCharArray();
-        StringBuilder initials = new StringBuilder();
-
-        for (char c : chars) {
-            if (Character.isUpperCase(c)) {
-                initials.append(c);
-            }
+        for (String s : name.split(" ")){
+            System.out.println(s.charAt(0));
         }
-
-        System.out.println(initials);
     }
 }
